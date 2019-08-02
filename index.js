93379725a25bc48f2e39aff2ce8e7f818c4dcc6c
Apple <apple@Apples-MacBook-Pro-147.local>
2019-08-02 14:20:31 -0400

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 console.log("linked");
 var button = $("button");
 var zipCode = $(".zip").val();
-var events = {90292 : ["Nothin' But Sand Beach Cleanup: Saturday, August 17 10am - 12pm", "CA-wide coastal cleanup: Saturday, September 21 9-12", "SoCal 350 general meeting: Sunday, August 18 3-5", "or email Ted Lieu for more ways to take action against climate change at info@tedlieu.com  P.O. Box 1309 Torrance, CA 90505"]};
+var events = {90292 : ["Nothin' But Sand Beach Cleanup: Saturday, August 17 10am - 12pm. ", "CA-wide coastal cleanup: Saturday, September 21 9-12. ", "SoCal 350 general meeting: Sunday, August 18 3-5. ", "email Ted Lieu for more ways to take action against climate change at info@tedlieu.com  P.O. Box 1309 Torrance, CA 90505. "]};
 var container = $(".container");
 
 //function Calls
